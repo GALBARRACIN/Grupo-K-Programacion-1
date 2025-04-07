8616b2d1858b8bdd25af569eac43223f2db9f010
AgusCorren <am.correnti@alumno.um.edu.ar>
2025-04-07 20:26:53 -0300

    Modificaciones y correciones

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from main import create_app
+from main import db
+
 import os
 
 app = create_app()
 
+
 app.app_context().push()
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT'))
--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,18 +2,28 @@ from flask import Flask
 from dotenv import load_dotenv
 
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
-import main.resources as resources
+import main.resources as resource
+import os
 
 #Inicializamos restful
 api = Api()
+db = SQLAlchemy()
 
 def create_app():
     #Inicializar flask
     app = Flask(__name__)
     #cargamos variables de entorno
     load_dotenv()
-       
+    
+    if not os.path.exists(os.getenv('DATABASE_NAME')):
+        os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_EXT'))
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
     #cargar los recursos
     api.add_resource(resources.AnimalResource, '/animal/<id>')
     api.add_resource(resources.AnimalesResource, '/animales')
